ca323fdbb91e1a4c535e2499540c951af6d3f96f
Daniel Alejandro Alvarez Vanegas <96156811+DuneV@users.noreply.github.com>
2023-04-15 21:09:35 -0500

    update

--- sub_ws/src/mov_woc/setup.py
+++ sub_ws/src/mov_woc/setup.py
@@ -12,7 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.py'))
+        (os.path.join('src/install',package_name,'share', package_name), glob('launch/*.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
